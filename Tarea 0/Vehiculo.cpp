7f8ae80659affff766ebf92efd21ba9f92c5742b
Franco-77 <39070378+Franco-77@users.noreply.github.com>
2019-03-13 19:29:15 -0300

    Create Vehiculo.cpp

---
+++ Tarea 0/Vehiculo.cpp
@@ -0,0 +1,50 @@
+#include "Vehiculo.hpp"
+
+//constructores
+Vehiculo::Vehiculo (){
+	new int nroSerie = 0;
+	new float porcentajeBateria = 0;
+    new float precioBase = 0;
+}
+
+Vehiculo::Vehiculo(int n, float p, float pb){
+    new int nroSerie = n;
+	new float porcentajeBateria = p;
+    new float precioBase = pb;
+}
+
+Vehiculo::~Vehiculo(){
+    delete nroSerie;
+    delete porcentajeBateria;
+    delete precioBase;
+}
+
+//set y get
+Vehiculo::setNroSerie(int n){
+    this.nroSerie = n;
+}
+
+Vehiculo::setPorcentajeBateria(float p){
+    this.porcentajeBateria = p;
+}
+
+Vehiculo::setPrecioBase(float pb){
+    this.precioBase = pb;
+}
+
+int Vehiculo::getNroSerie(){
+    return nroSerie;
+}
+
+float Vehiculo::getPorcentajeBateria(){
+    return porcentajeBateria;
+}
+
+float Vehiculo::getPrecioBase(){
+    return precioBase;
+}
+
+//
+
+ float Vehiculo::darPrecioViaje(int duracion, int distancia){
+ }

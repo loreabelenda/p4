9e4ddb2d92754134ed6e0f0137702999ea368aca
loreabelenda <loreabelenda@gmail.com>
2019-03-16 11:35:28 -0300

    Primer_draft.cpp

---
+++ Tarea0/funcionesLorenzo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Vehiculo.h"
+#include "DtViajeBase.h"
+
+using namespace std;
+
+void ingresarViaje(string ci, int nroSerieVehiculo, const DtViajeBase& viaje){
+  
+}
+
+DtViaje** verViajesAntesDeFecha(const DtFecha& fecha, string ci, int& cantViajes){
+
+}
